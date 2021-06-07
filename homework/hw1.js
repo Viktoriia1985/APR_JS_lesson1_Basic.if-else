18cb2e0e2cd1f08ce8f089a3a194fb58e007e935
Viktoriia Krat <flightsspb@gmail.com>
2021-06-07 14:45:44 +0300

    Initial commit

---
+++ homework/hw1.js
@@ -0,0 +1,597 @@
+//                      H O M E W O R K _ 1
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7,
+// 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let name = 'hello';
+// let name1 = 'owu';
+// let name2 = 'com';
+// let name3 = 'ua';
+// let num1 = 1;
+// let num2 = -999;
+// let num3 = 123;
+// let PI = 3.14;
+// let num4 = 2.7;
+// let num5 = 16;
+// let bool1 = true;
+// let bool2 = false;
+//
+// console.log(name);
+// alert(name);
+// document.write(name);
+// document.write('<br>');
+//
+// console.log(name1);
+// alert(name1);
+// document.write(name1);
+// document.write('<br>');
+//
+// console.log(name2);
+// alert(name2);
+// document.write(name2);
+// document.write('<br>');
+//
+// console.log(name3);
+// alert(name3);
+// document.write(name3);
+// document.write('<br>');
+//
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+// document.write('<br>');
+//
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+// document.write('<br>');
+//
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+// document.write('<br>');
+//
+// console.log(PI);
+// alert(PI);
+// document.write(PI);
+// document.write('<br>');
+//
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+// document.write('<br>');
+//
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+// document.write('<br>');
+//
+// console.log(bool1);
+// alert(bool1);
+// document.write(bool1);
+// document.write('<br>');
+//
+// console.log(bool2);
+// alert(bool2);
+// document.write(bool2);
+// document.write('<br>');
+
+// - Переприсвоїти кожну змінну з завдання вище на будь які довільні значення.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+//  name = 'Good morning';
+//  name1 = 'Ok10';
+//  name2 = 'School';
+//  name3 = 'Ukraine';
+//  num1 = 100;
+//  num2 = -19.56;
+//  num3 = 7;
+//  PI = 3.14;
+//  num4 = 212.7;
+//  num5 = -216;
+//  bool1 = false;
+//  bool2 = true;
+//
+// console.log(name);
+// alert(name);
+// document.write(name);
+// document.write('<br>');
+//
+// console.log(name1);
+// alert(name1);
+// document.write(name1);
+// document.write('<br>');
+//
+// console.log(name2);
+// alert(name2);
+// document.write(name2);
+// document.write('<br>');
+//
+// console.log(name3);
+// alert(name3);
+// document.write(name3);
+// document.write('<br>');
+//
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+// document.write('<br>');
+//
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+// document.write('<br>');
+//
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+// document.write('<br>');
+//
+// console.log(PI);
+// alert(PI);
+// document.write(PI);
+// document.write('<br>');
+//
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+// document.write('<br>');
+//
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+// document.write('<br>');
+//
+// console.log(bool1);
+// alert(bool1);
+// document.write(bool1);
+// document.write('<br>');
+//
+// console.log(bool2);
+// alert(bool2);
+// document.write(bool2);
+// document.write('<br>');
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+//
+// let name = prompt('What is your name?', 'Viktoriia');
+// let familyName = prompt('What is your familyName?','Valeriyivna');
+// let age = +prompt('What is your age?', 36);
+// document.write(`<div>Вітаю ${name} ${familyName}. Тобі ${age} років.</div>`);
+//
+// - Взяти змінні з завдання вище та зєднати їх в одну змінну (конкатинація).
+//
+// let newPerson = name + ' ' + familyName + ' ' + age;
+// console.log(newPerson);
+// alert(newPerson);
+// document.write(newPerson);
+
+// - За допомогою prompt() отримати 3 числа. Привести їх до числа (Google "String to number JS"). Вивести їх в консоль.
+//
+// let num1 = +prompt('What is your number 1?');
+// let num2 = +prompt('What is your number 2?');
+// let num3 = +prompt('What is your number 3?');
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+//
+//            OR
+//
+// let num4 = parseInt(prompt('What is your number?'));
+// let num5 = parseInt(prompt('What is your number?'));
+// let num6 = parseInt(prompt('What is your number?'));
+//
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+//           OR
+//
+// let num7 = parseFloat(prompt('What is your number?'));
+// let num8 = parseFloat(prompt('What is your number?'));
+// let num9 = parseFloat(prompt('What is your number?'));
+//
+// console.log(num7);
+// console.log(num8);
+// console.log(num9);
+//           OR
+//
+// let num10 = Number(prompt('What is your number?'));
+// let num11 = Number(prompt('What is your number?'));
+// let num12 = Number(prompt('What is your number?'));
+//
+// console.log(num10);
+// console.log(num11);
+// console.log(num12);
+
+// - За допомогою prompt() отримати 4 числа. Привести їх до числа за допомогою parseInt. Додати їх всі між собою і
+// результат вивести в консоль.
+//
+// let num1 = parseInt(prompt('What is your number?', 2));
+// let num2 = parseInt(prompt('What is your number?', 4));
+// let num3 = parseInt(prompt('What is your number?', 6));
+// let num4 = parseInt(prompt('What is your number?', 8));
+//
+// let sum = num1 + num2 + num3 + num4;
+// console.log(sum);
+
+// - За допомогою prompt() отримати 3 числа з плаваючою точкою за допомогою parseFloat.
+//   Просумувати їх. Отрманий результат записати в змінну result. Вивести цю змінну в консоль.
+//
+// let num1 = parseFloat(prompt('What is your number?', 2.45));
+// let num2 = parseFloat(prompt('What is your number?', 10.3));
+// let num3 = parseFloat(prompt('What is your number?', 5.567));
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+//
+// let sum = num1 + num2 + num3;
+// console.log(sum);
+
+// - За допомогою prompt() отримати 3 числа з плаваючою точкою. Заокруглити їх (Google "round number JS").
+//  Просумувати їх між собою. Результат вивести в консоль.
+//
+// let num1 = parseFloat(prompt('What is your number?', 2.56));
+// let num2 = parseFloat(prompt('What is your number?', 4.05));
+// let num3 = parseFloat(prompt('What is your number?', 10.15));
+//
+// let sum = Math.round(num1 + num2 + num3);
+// console.log(sum);
+
+// - За допомогою prompt() отримати 2 цілих числа.
+//     Перше число - це число яке будуть підносити до степеня. Друге число - це число яке являється степенню.
+//     За допомогою Math.pow (Google) піднести перше число в степінь другого числа.
+//     Example. Число 1 = 5. Число 2 = 2. Результат = 25
+//
+// let a = +prompt('Write the number');
+// let b = +prompt('Write the pow');
+//
+// let result = Math.pow(a, b);
+// console.log(result);
+
+//     - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// - Поставте відповідний оператор в виразах, щоб вийшов відповідний результат.
+//    В однакових виразах не використовувати однакові оператори!!!
+//
+// 5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+//
+// console.log(5 < 6, 5 !== 6);
+// console.log(5 > 6);
+// console.log(5 === 6);
+// console.log(5 == 6);
+//
+// console.log(10 === 10);
+// console.log(10 == 10);
+//
+// console.log(10 !== 10);
+// console.log(10 > 10);
+// console.log(10 <10);
+//
+// console.log(123 === '123');
+// console.log(123 == '123');
+
+//------------------------------------------------------------------------------------------------------------
+//                                                    Додатково:
+//------------------------------------------------------------------------------------------------------------
+
+//   - Подивіться на наступні вирази і спробуйте вгадати яким буде вивід в консоль. Перевірте себе.
+// console.log(132 > 100 && 45 < 12 ); // false
+// console.log(34 > 33 && 23 < 90 );  // true
+// console.log(99 > 100 && 45 > 12 );  // false
+// console.log(132 > 100 || 45 < 12 ); // true
+// console.log(111 > 11 || 45 < 111 ); // true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); // true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); // true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); // false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); // true
+// console.log(!!'-1'); // true
+// console.log(!!-1);  // true
+// console.log(!!'0');  // true
+// console.log(!!'null'); // true
+// console.log(!!'undefined'); // true
+// console.log(!!(3/'owu'));  // false
+// console.log((111 > 11 || 45 < 111) ||  !!'0'); // true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); // false
+//
+//
+//---------------------------------------------------------------------------------------------------------------------------------
+// ______________________________________________________________________________________________________________________________________________________
+//                                                 РОБОТА В АУДИТОРІЇ
+// ______________________________________________________________________________________________________________________________________________________
+//
+//-----------------------------------------------------------------------------------------------------------------------------
+
+// 1. Оголосіть змінні str, num, flag и txt зі значеннями «Привіт», 123, true, «true».
+// За допомогою typeof переконайтесь, що значення змінних належать типам: string, number и  boolean.
+//
+// let str = 'Привіт';
+// let num = 123;
+// let flag = true;
+// let txt = 'true';
+//
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+
+
+// 2. Оголосіть змінні a1, a2, a3, a4, a5. За домогою 3х математичних операцій отримайте числа:
+//         7,
+//         22,
+//         102,
+//         5,
+//         72
+// Старайтесь використовувати різні оператори.
+//     Example: 88 = (16 / 2) * 11
+//
+// let a1 = 5;
+// let a2 = 10;
+// let a3 = 7;
+// let a4 = 11;
+// let a5 = 3;
+//
+// let num1 = (a4 + a2) / a5;
+// console.log(num1);
+//
+// let num2 = (a4 * a2) / a1;
+// console.log(num2);
+//
+// let num3 = (a3 + a2) * (a4 - a1);
+// console.log(num3);
+//
+// let num4 = (a1 + a2) / a5;
+// console.log(num4);
+//
+// let num5 = (a4 + a2 + a5) * a5;
+// console.log(num5);
+
+
+// 3.  Створіть змінні a6, a7, a8, a9, a10. Запишіть в них результати виразів:
+//     5 % 3,
+//     3 % 5,
+//     5 + '3',
+//     '5' - 3,
+//     75 + 'кг'
+//
+// let a1 = (5 % 3); //  2 number
+// console.log(a1);
+//
+// let a2 = (3 % 5); //  3 number
+// console.log(a2);
+//
+// let a3 = (5 + '3');  //  53 string
+// console.log(a3);
+//
+// let a4 = ('5' - 3);  // 2 number
+// console.log(a4);
+//
+// let a5 = (75 + 'кг');  //  75кг string
+// console.log(a5);
+
+
+// 4. Напищіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+//
+// let height = 23;
+// let width = 10;
+// let s = height * width;
+// console.log(s);
+
+// 5.  Напиши код, который находит объем цилиндра высотой 10м (переменная heightC) и диаметром основания 4м (dC),
+//     результат поместите в переменную v. (V = P * rC*rC * heightC);
+// let heightC = 10;
+// let dC = 4;
+// let rC = dC / 2; // радіус циліндра
+// const P = 3.14;
+// let V = P * (rC*rC) * heightC;
+// console.log(V);
+
+
+// 6. У прямоугольного треугольника две стороны n (со значением 3) и m (со значением 4).
+// Найдите гипотенузу k по теореме Пифагора (нужно использовать функцию Math.pow(число, степень) или оператор
+// возведения в степень ** ).   (formula:  k*k = n*n + m*m);
+//
+// let n = 3;
+// let m = 4;
+//
+// let k = Math.sqrt(Math.pow(3,2) + Math.pow(4,2));
+// console.log(k);
+//
+// let k1 = Math.sqrt(n*n + m*m);
+// console.log(k1);
+
+
+// 7. Напишите скрипт, который выводит "Hello world", создавши переменную str и выводить спомощью document.write,
+// alert и console.log
+// let str = 'Hello world';
+// console.log('Hello world');
+// alert('Hello world');
+// document.write('Hello world');
+
+// 8. Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби
+// (каждой на новой строки спомощью \n).
+//
+// let surname = 'Krat';
+// let name = 'Viktoriia';
+// let familyName = 'Valeriyivna';
+// let age = 36;
+// let hobby = 'Java Script';
+//
+// alert(surname + '\n' + name + '\n' + familyName + '\n' + age + '\n' + hobby);
+// alert(`${surname} \n ${name} \n ${familyName} \n ${age} \n ${hobby}`);
+
+
+// 9. Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
+//     Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’
+//   Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
+//     Вывести в документ содержимое переменной concatenation спомощью document.write
+//
+// let str1 = 'Кто';
+// let str2 = 'ты';
+// let str3 = 'такой?';
+//
+// let concatenation = str1 + ' ' + str2 + ' ' + str3;
+// console.log(concatenation);
+// document.write(concatenation);
+
+
+// 10. Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br>");
+// console.log(typeof (str + a));  // 205 string
+// document.write(str - a + "<br>");
+// console.log(typeof(str - a));   // 15 number
+// document.write(str * "2" + "<br>");
+// console.log(typeof(str * "2"));  // 40 number
+// document.write(str / 2 + "<br>");
+// console.log(typeof(str / 2));  //   10 number
+
+// 11. Какие значения выведет в окно консоли следующий фрагмент кода если его поместить в console.log?
+// console.log(parseInt("3.14")); // 3 number
+// console.log(parseInt("-7.875"));  // -7 number
+// console.log(parseInt("435")); // 435 number
+// console.log(parseInt("Вася")); // NaN
+
+
+// 12.  С помощью окна ввода, вызываемого методом prompt, пользователь может ввести данные,
+// которые будут использоваться далее, повторите код ниже
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str);
+
+// 13. С помощью окна ввода, вызываемого методом prompt, сделать сложение двух чисел, а вывод результата
+// при помощи метода alert
+// let num1 = +prompt('Number 1');
+// let num2 = +prompt('Number 2');
+// alert(num1 + num2);
+
+// 14. С помощью окна ввода, вызываемого методом prompt, пользователь последовательно выводит имя, фамилию и
+// возраст, а вам не обходимо вывести строку такого вида
+// Доброго вечера Иван Иванов, мои поздравления что вам 32 , а вывод результата при помощи метода alert
+//
+// let name = prompt('Name', 'Иван');
+// let surname = prompt('Surname', 'Иванов');
+// let age = prompt('Age', 32);
+//
+// alert(`Добрый вечер ${name} ${surname}.  Вам ${age} года.`)
+
+// =====================
+// ======ДОП============
+// =====================
+
+
+// 1. Три різних числа вводяться через prompt().
+//     За допомоги if else вивести іх в порядку зростання. (відсортувати по зростанню)
+//
+//
+// let a = prompt('Number 1', 20);
+// let b = prompt('Number 2', 15);
+// let c = prompt('Number 3', 8);
+//
+// if (a > b && a > c) {
+//     if (b > c)
+//         console.log(c, b, a);
+// } else {
+//     console.log(c, b, a);
+// }
+//         OR
+//
+// a = +prompt();
+// b = +prompt();
+// c = +prompt();
+//
+// if (a > b && a > c ) {
+//     if (b > c) {
+//         console.log(c, b, a);
+//     } else {
+//         console.log(b, c, a);
+//     }
+// }
+// if (b > a && b > c) {
+//     if (a > c) {
+//         console.log(c, a, b);
+//     } else  {
+//         console.log(a, c, b);
+//     }
+// }
+// if (c > a && c > b) {
+//     if (a > b) {
+//         console.log(b, a, c);
+//     } else {
+//         console.log(a, b, c)
+//     }
+// }
+
+
+// 2.
+// Все параматры получаем с клавиатуры!!!!
+//     Имитируем поведение пешехода на перекстке.
+//     Если светофор зеленый - вывести "иди".
+//     Если светофор желтый - вывести "подожди".
+//     Если светофор красный - вывести "стой".
+//     Если светофор в аварийном режиме вывести "делай что хочешь"!
+//
+// let color = prompt('Enter a color');
+// if (color === 'green') {
+//     console.log('GO!');
+// } else if (color === 'yellow') {
+//     console.log('WAIT');
+// } else if(color === 'red') {
+//     console.log('STOP!')
+// } else {
+//     console.log("Don't be afraid")
+// }
+
+
+//     3
+// Все параметры получаем с клавиатуры!!!!(prompt , confirm)
+// Создать переменную isRoadClear которая характеризирует наличие на дороге машин.
+//     Улучшаем предыдущее задание.
+//     Если светофор зеленый и машин нет - вывести "иди".
+//     Если светофор зеленый и машины есть  - вывести подожди пока нарушители проедут".
+//      Если светофор желтый и машины есть - вывести "жди".
+//     Если светофор желтый и машин нет - вывести "все рано жди".
+//     Если светофор красный и машин нет- вывести "стой все рано".
+//     Если светофор красный - и машины есть вывести "стой и жди".
+//     Если светофор в аварийном режиме вывести "делай что хочешь"!
+//
+// const isRoadClear = confirm('Is road clear?');
+// let color = prompt('What is a color?');
+//
+// switch (color) {
+//     case 'green':
+//         isRoadClear ? console.log('GO GO GO!') : console.log('Wait the cars passed');
+//         break;
+//     case 'yellow':
+//         isRoadClear ? console.log('Go') : console.log('Wait the cars passed');
+//         break;
+//     case 'red':
+//         isRoadClear ? console.log('STOP, Do not GO!') : console.log('STOP STOP STOP!!!');
+//         break;
+//     default:
+//         alert ('Wrong Color!');
+// };
+
+
